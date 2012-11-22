326e83f7d446c8090a6a74e64fc3f3951c7b519a
mr.datel@gmail.com <mr.datel@gmail.com@37c588c5-a372-728e-393a-ba12e7ad614d>
2012-11-22 21:25:16 +0000

    Přidán projekt pro DashBoard

---
+++ trunk/ meteo-arduino-mega/DashBoard/Window2.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Timers;
+
+namespace WindowTransparency
+{
+  /// <summary>
+  /// Interaction logic for Window2.xaml
+  /// </summary>
+  public partial class Window2 : Window
+  {
+    Timer t = new Timer(1000);
+    
+    public Window2()
+    {
+      InitializeComponent();
+      t.Start();
+    }
+
+
+    private void rectangle1_MouseEnter(object sender, MouseEventArgs e)
+    {
+      //Rectangle rec = (Rectangle)sender;
+      //string name = rec.Name;
+      rectangle1.Height = 250;
+    }
+
+    private void rectangle1_MouseLeave(object sender, MouseEventArgs e)
+    {
+      rectangle1.Height = 50;
+
+    }
+
+  
+  }
+}

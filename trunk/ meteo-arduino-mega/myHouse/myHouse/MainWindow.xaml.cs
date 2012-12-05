bb0a9993db771ea0617a33fb06890c625a382bef
mr.datel@gmail.com <mr.datel@gmail.com@37c588c5-a372-728e-393a-ba12e7ad614d>
2012-12-05 21:32:42 +0000

    Opravena chyba pokud udaj neni cislo.

--- trunk/ meteo-arduino-mega/myHouse/myHouse/MainWindow.xaml.cs
+++ trunk/ meteo-arduino-mega/myHouse/myHouse/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Windows;
 using System.Timers;
 using System.ComponentModel;
 using System.Windows.Media;
+using System.Windows.Markup;
+using System.Globalization;
 
 namespace myHouse
 {
@@ -19,6 +21,7 @@ namespace myHouse
         public MainWindow()
         {
             InitializeComponent();
+            this.Language = XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag);
             DataContext = hd;
             timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
             timer.Enabled = true;
@@ -70,33 +73,43 @@ namespace myHouse
             
              */
             string[] radka = data.Split(new Char[] { '\n' });
+            hd.resetTemperatures();
             foreach (string s in radka)
             {
                 string[] column = s.Split(new Char[] { ',' });
-                if (column[0] == "T28C9B84104000097")
+                try
                 {
-                    hd.corridorTemp = (float)Convert.ToDecimal(column[2].Replace(".", ","));
-                    hd.corridorColor = getRoomColor(hd.corridorTemp);
+                    if (column[0] == "T28C9B84104000097")
+                    {
+                        hd.corridorTemp = (float)Convert.ToDecimal(column[2].Replace(".", ","));
+                        hd.corridorColor = getRoomColor(hd.corridorTemp);
+                    }
+                    if (column[0] == "T28AEC04104000082")
+                    {
+                        hd.hallTemp = (float)Convert.ToDecimal(column[2].Replace(".", ","));
+                        hd.hallColor = getRoomColor(hd.hallTemp);
+                    }
+                    if (column[0] == "T28A6B0410400004E")
+                    {
+                        hd.livingRoomTemp = (float)Convert.ToDecimal(column[2].Replace(".", ","));
+                        hd.livingRoomColor = getRoomColor(hd.livingRoomTemp);
+                    }
+                    if (column[0] == "T28CEB0410400002C")
+                    {
+                        hd.workRoomTemp = (float)Convert.ToDecimal(column[2].Replace(".", ","));
+                        hd.workRoomColor = getRoomColor(hd.workRoomTemp);
+                    }
+                    if (column[0] == "T28E8B84104000016")
+                    {
+                        hd.bedRoomTemp = (float)Convert.ToDecimal(column[2].Replace(".", ","));
+                        hd.bedRoomColor = getRoomColor(hd.workRoomTemp);
+                    }
+                    hd.lastUpdate = getDateTimeFromCosmString(column[1] + column[2]);
+
                 }
-                if (column[0] == "T28AEC04104000082")
+                catch (FormatException ex)
                 {
-                    hd.hallTemp = (float)Convert.ToDecimal(column[2].Replace(".", ","));
-                    hd.hallColor = getRoomColor(hd.hallTemp);
-                }
-                if (column[0] == "T28A6B0410400004E")
-                {
-                    hd.livingRoomTemp = (float)Convert.ToDecimal(column[2].Replace(".", ","));
-                    hd.livingRoomColor = getRoomColor(hd.livingRoomTemp);
-                }
-                if (column[0] == "T28CEB0410400002C")
-                {
-                    hd.workRoomTemp = (float)Convert.ToDecimal(column[2].Replace(".", ","));
-                    hd.workRoomColor = getRoomColor(hd.workRoomTemp);
-                }
-                if (column[0] == "T28E8B84104000016")
-                {
-                    hd.bedRoomTemp = (float)Convert.ToDecimal(column[2].Replace(".", ","));
-                    hd.bedRoomColor = getRoomColor(hd.workRoomTemp);
+
                 }
                 //lastUpdate = getDateTimeFromCosmString(column[1] + column[2]);
             }
@@ -112,6 +125,18 @@ namespace myHouse
                 return Colors.AliceBlue.ToString();
             else
                 return Colors.Blue.ToString();
+        }
+
+        private DateTime getDateTimeFromCosmString(string s)
+        {
+            if (s == string.Empty)
+                return new DateTime();
+            String[] dateAndTime = s.Split(new Char[] { 'T' });
+            String[] date = dateAndTime[0].Split(new Char[] { '-' });
+            String[] time = dateAndTime[1].Split(new Char[] { ':' });
+
+            return new DateTime(Convert.ToInt16(date[0]), Convert.ToInt16(date[1]), Convert.ToInt16(date[2]), Convert.ToInt16(time[0]), Convert.ToInt16(time[1]), Convert.ToInt16(time[2].Substring(0, time[2].IndexOf('.'))));
+
         }
     }
 
@@ -136,9 +161,15 @@ namespace myHouse
         private string _workRoomColor;
         private string _bedRoomColor;
         private string _statusLEDColor;
+        private DateTime _lastUpdate;
 
         public HouseData()
         {
+        }
+
+        public void resetTemperatures() {
+            bedRoomTemp = corridorTemp = hallTemp = workRoomTemp = livingRoomTemp = -127;
+            bedRoomColor = corridorColor = hallColor = workRoomColor = livingRoomColor = Colors.Red.ToString();
         }
 
         public float corridorTemp { get { return _corridorTemp; } set { _corridorTemp = value; NotifyPropertyChanged("corridorTemp");}}
@@ -154,5 +185,8 @@ namespace myHouse
         public string bedRoomColor { get { return _bedRoomColor.ToString(); } set { _bedRoomColor = value; NotifyPropertyChanged("bedRoomColor"); } }
 
         public string statusLEDColor { get { return _statusLEDColor; } set { _statusLEDColor = value; NotifyPropertyChanged("statusLEDColor");}}
+
+        public DateTime lastUpdate { get { return _lastUpdate; } set { _lastUpdate = value; NotifyPropertyChanged("lastUpdate"); } }
+
     }
 }
